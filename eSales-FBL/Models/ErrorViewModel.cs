32a7ebff3aa9c0d0907d2592d9f16cd3d27043b8
Saravana Kumar <gsaravanakumar@ebms.co.in>
2022-08-13 00:34:06 +0530

    Add project files.

---
+++ eSales-FBL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eSales_FBL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
